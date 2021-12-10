2c26b7a46a0200e40513a764f8b73387007f8e62
Dawid Skarwecki <dawidskarwecki@gmail.com>
2021-12-10 19:58:20 +0100

    update js playerMove and ComputerMove

--- js/script.js
+++ js/script.js
@@ -10,4 +10,18 @@ if(randomNumber=='1'){
     computerMove='nozyczki';
 
 }
-printMessage('MOj ruch to ' + computerMove);
+printMessage('Moj ruch to ' + computerMove);
+var playerMove, playerInput;
+playerInput=prompt('Wybierz swoj ruch 1:kamien, 2:papier, 3:nozyczki');
+console.log('Wybrany ruch to'+playerInput);
+if(playerInput=='1'){
+    playerMove='kamien';
+}else if(playerInput=='2'){
+    playerMove='papier';
+} else if(playerInput=='3'){
+    playerMove='nozyczki'
+} else {
+    printMessage('Niedozwolony ruch');
+    playerMove='kamien';
+}
+printMessage('Twoj ruch to ' + playerMove)
